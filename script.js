1027f32840f79bdfa6f28e8e0a623af63013b445
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-07-04 16:41:09 +0530

    Practice branch command

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-let btn = document.querySelector(".btn");
-let box = document.querySelector(".div1");
-let isclick = false;
-btn.addEventListener("click", (e) => {
-  if (isclick) {
-    box.style.backgroundColor = "orange";
-  } else {
-    box.style.backgroundColor = "black";
-  }
-  isclick = !isclick;
-});
+// let btn = document.querySelector(".btn");
+// let box = document.querySelector(".div1");
+// let isclick = false;
+// btn.addEventListener("click", (e) => {
+//   if (isclick) {
+//     box.style.backgroundColor = "orange";
+//   } else {
+//     box.style.backgroundColor = "black";
+//   }
+//   isclick = !isclick;
+// });
+
+// practice purpose
+
+let a = 10;
+let b = 12;
+let temp;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log("a:", a);
+console.log("b:", b);
