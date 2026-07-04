33c67ce5d033c1714f319f4df7051fa50465bb20
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-07-04 16:45:22 +0530

    Remove comments from main code

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// let btn = document.querySelector(".btn");
-// let box = document.querySelector(".div1");
-// let isclick = false;
-// btn.addEventListener("click", (e) => {
-//   if (isclick) {
-//     box.style.backgroundColor = "orange";
-//   } else {
-//     box.style.backgroundColor = "black";
-//   }
-//   isclick = !isclick;
-// });
+let btn = document.querySelector(".btn");
+let box = document.querySelector(".div1");
+let isclick = false;
+btn.addEventListener("click", (e) => {
+  if (isclick) {
+    box.style.backgroundColor = "orange";
+  } else {
+    box.style.backgroundColor = "black";
+  }
+  isclick = !isclick;
+});
 
 // practice purpose
 
