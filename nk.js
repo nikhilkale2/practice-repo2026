b0683b938aaec5ebbe1208d324c5408363c0ea08
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-07-06 13:43:09 +0530

    Create a new file to practice new program

---
+++ nk.js
@@ -0,0 +1,9 @@
+let str = "b";
+
+if (str == "a" || str == "e" || str == "i" || str == "o" || str == "u") {
+  console.log("vowel");
+} else {
+  console.log("consonant");
+}
+
+// ---------------------------------------------//
